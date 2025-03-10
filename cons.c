30cd992d8c63d6faf562a02e59b0fef0e5949dd9
astralcaleo <hugogilsanz@icloud.com>
2025-03-10 11:19:15 +0100

    A

---
+++ cons.c
@@ -0,0 +1,26 @@
+// Don Asier Cabo (que no Caba) Lodeiro y Hugo Gilsanz Ortellado
+#include <stdio.h>
+#include <stdlib.h>
+#define N 8
+
+void sleep(){
+
+}
+
+void wakeup(){
+
+}
+
+// Función que retira del buffer el caracter que corresponda y lo devuelve
+char remove_item(char* buffer){
+}
+
+// Función que añade el caracter retirado del buffer a un string local (llama a remove_item())
+void consume_item(char* local){
+}
+
+int main(int argc, char const *argv[])
+{
+    char* buffer;
+    return 0;
+}
---
+++ prod.c
@@ -0,0 +1,26 @@
+// Don Asier Cabo (que no Caba) Lodeiro y Hugo Gilsanz Ortellado
+#include <stdio.h>
+#include <stdlib.h>
+#define N 8
+
+void sleep(){
+
+}
+
+void wakeup(){
+
+}
+
+// Función que genera una mayúscula aleatoria
+char produce_item(){
+}
+
+// Función que recibe un caracter y lo añade a la cadena pasada por referencia
+void insert_item(char caracter, char* cadena){
+}
+
+int main(int argc, char const *argv[])
+{
+    char* buffer;
+    return 0;
+}
